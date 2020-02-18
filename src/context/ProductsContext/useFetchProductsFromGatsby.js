370abacc2be68880874cbba7b522d6f48999b39f
Lorenzo GM <dev@lorenzogm.com>
2020-02-18 14:24:46 +0100

    fix(graphql): images max width to 800

--- src/context/ProductsContext/useFetchProductsFromGatsby.js
+++ src/context/ProductsContext/useFetchProductsFromGatsby.js
@@ -6,7 +6,7 @@ import getProducts from './getProducts'
 /** Normalize structure of data sourced from Gatsby's GraphQL store */
 const useFetchProductsFromGatsby = () => {
   const { allStripeSku } = useStaticQuery(graphql`
-    query allStripeSku($maxWidth: Int = 300, $quality: Int = 92) {
+    query allStripeSku($maxWidth: Int = 800, $quality: Int = 92) {
       allStripeSku(filter: { product: { active: { eq: true } } }) {
         edges {
           node {
--- src/graphql/sku/allStripeSku.js
+++ src/graphql/sku/allStripeSku.js
@@ -1,7 +1,7 @@
 import { graphql } from 'gatsby'
 
 const allStripeSku = graphql`
-  query allStripeSku($maxWidth: Int = 300, $quality: Int = 92) {
+  query allStripeSku($maxWidth: Int = 800, $quality: Int = 92) {
     allStripeSku(filter: { product: { active: { eq: true } } }) {
       group(field: product___id) {
         fieldValue
