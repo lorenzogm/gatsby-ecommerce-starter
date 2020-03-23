e07db7b6a612a4b0a7708d76fb49881233152598
Lorenzo GM <dev@lorenzogm.com>
2020-03-23 16:22:32 +0100

    fix(functions): add loop in "stripeSkusList"

--- src/functions/stripeSkusList.js
+++ src/functions/stripeSkusList.js
@@ -3,25 +3,39 @@ const stripe = require('../services/stripe')
 /**
  * Returns list of skus with product fields expanded.
  */
-module.exports.handler = (event, context, callback) => {
-  stripe.skus.list(
-    {
-      active: true,
-      limit: 100, // max allowed
-      expand: ['data.product'],
-    },
-    (err, result) => {
-      const response = {
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
-      }
 
-      if (err) {
-        callback(null, { ...response, statusCode: 500, body: JSON.stringify({ error: err.message }) })
-      } else {
-        callback(null, { ...response, statusCode: 200, body: JSON.stringify({ data: result.data }) })
-      }
+const stripeSkusList = async ({ skusList }) => {
+  // eslint-disable-next-line no-await-in-loop
+  const { data, has_more: hasMore } = await stripe.skus.list({
+    active: true,
+    limit: 100, // max allowed
+    expand: ['data.product'],
+    starting_after: skusList.length === 0 ? undefined : skusList[skusList.length - 1].id,
+  })
+
+  // eslint-disable-next-line no-console
+  console.log(`${skusList.length} skus loaded`)
+
+  const skusListNew = [...skusList, ...data]
+
+  if (hasMore === false) {
+    return { skusList: skusListNew }
+  }
+
+  return stripeSkusList({ skusList: skusListNew })
+}
+
+module.exports.handler = async () => {
+  const response = {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
     },
-  )
+  }
+
+  try {
+    const { skusList } = await stripeSkusList({ skusList: [] })
+    return { ...response, statusCode: 200, body: JSON.stringify({ data: skusList }) }
+  } catch (error) {
+    return { ...response, statusCode: 500, body: JSON.stringify({ error: error.message }) }
+  }
 }
