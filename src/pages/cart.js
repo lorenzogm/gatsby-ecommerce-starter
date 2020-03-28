32c7d2dafda61e614047811bf5c1991b00965216
Lorenzo GM <dev@lorenzogm.com>
2020-03-28 14:35:48 +0100

    fix(pages): move pages out of "pages" folders to avoid erros when building

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,3 +1,3 @@
-import CartPage from './CartPage'
+import CartPage from '../pagesCustom/CartPage'
 
 export default CartPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,3 @@
-import HomePage from './HomePage'
+import HomePage from '../pagesCustom/HomePage'
 
 export default HomePage
