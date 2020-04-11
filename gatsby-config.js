e84eb8713bd44b179d5034b5e88d33de5b9a89aa
Lorenzo García Moreno <lorenzo.garciamoreno@valtech.com>
2020-04-11 19:01:17 +0200

    fix(config): firebase credentials

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,6 +73,16 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-firebase',
       options: {
+        credentials: {
+          apiKey: process.env.FIREBASE_API_KEY,
+          appId: process.env.FIREBASE_APP_ID,
+          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          databaseURL: process.env.FIREBASE_DATABASE_URL,
+          measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        },
         features: {
           analytics: true,
           auth: false,
