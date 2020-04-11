516def62c3fd19b83d7c6fd1b402263c7e43a265
Lorenzo García Moreno <lorenzo.garciamoreno@valtech.com>
2020-04-11 07:16:56 +0200

    refactor(CartPage): improve mobile design

--- src/templates/CartPage/index.js
+++ src/templates/CartPage/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
+import Divider from '@material-ui/core/Divider'
 
 import SEO from 'components/SEO'
 import { useCartContext } from 'context/CartContext'
@@ -24,17 +25,32 @@ const CartPage = () => {
       <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
 
       <Grid container spacing={3} direction={isMobile ? 'column' : 'row'}>
-        <Grid item xs={isMobile ? 12 : 8}>
-          <S.Paper elevation={2}>
-            <CartTitle />
-            <CartList />
-          </S.Paper>
-        </Grid>
-        <Grid item xs={isMobile ? 12 : 4}>
-          <S.Paper elevation={2}>
-            <CartSummary />
-          </S.Paper>
-        </Grid>
+        {isMobile ? (
+          <>
+            <Grid item xs={12}>
+              <CartTitle />
+              <CartList />
+            </Grid>
+            <Divider />
+            <Grid item xs={12}>
+              <CartSummary />
+            </Grid>
+          </>
+        ) : (
+          <>
+            <Grid item xs={8}>
+              <S.Paper elevation={2}>
+                <CartTitle />
+                <CartList />
+              </S.Paper>
+            </Grid>
+            <Grid item xs={4}>
+              <S.Paper elevation={2}>
+                <CartSummary />
+              </S.Paper>
+            </Grid>
+          </>
+        )}
       </Grid>
     </>
   )
