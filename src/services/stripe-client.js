9d6f65320527953aa1ff285868e435ef57ccc1a2
Lorenzo García Moreno <lorenzo.garciamoreno@valtech.com>
2020-07-02 21:45:23 +0200

    fix: load stripe client as singleton

--- src/services/stripe-client.js
+++ src/services/stripe-client.js
@@ -1,5 +1,14 @@
+/**
+ * This is a singleton to ensure we only instantiate Stripe once.
+ */
 import { loadStripe } from '@stripe/stripe-js'
 
-const stripe = async () => loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY)
+let stripePromise
+const stripe = () => {
+  if (!stripePromise) {
+    stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY)
+  }
+  return stripePromise
+}
 
 export default stripe
