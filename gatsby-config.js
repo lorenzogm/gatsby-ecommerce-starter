34bae066c677632b5ae540a6c7f85f77960688a0
Lorenzo García Moreno <lorenzo.garciamoreno@valtech.com>
2020-07-02 21:29:21 +0200

    update env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,7 @@ module.exports = {
       resolve: 'gatsby-source-stripe',
       options: {
         objects: ['Product', 'Sku'],
-        secretKey: process.env.GATSBY_STRIPE_SECRET_KEY,
+        secretKey: process.env.STRIPE_SECRET_KEY,
         downloadFiles: true,
         auth: false,
       },
@@ -74,14 +74,14 @@ module.exports = {
       resolve: 'gatsby-plugin-firebase',
       options: {
         credentials: {
-          apiKey: process.env.GATSBY_FIREBASE_API_KEY,
-          appId: process.env.GATSBY_FIREBASE_APP_ID,
-          authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
-          databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
-          measurementId: process.env.GATSBY_FIREBASE_MEASUREMENT_ID,
-          messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
-          projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
-          storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+          apiKey: process.env.FIREBASE_API_KEY,
+          appId: process.env.FIREBASE_APP_ID,
+          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          databaseURL: process.env.FIREBASE_DATABASE_URL,
+          measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
         },
         features: {
           analytics: true,
