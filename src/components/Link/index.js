6b96793022df1981b939956a876affdc6b783dbe
Lorenzo García Moreno <dev@lorenzogm.com>
2020-09-16 20:11:12 +0200

    feat(Link): add language prop

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -3,11 +3,20 @@ import PropTypes from 'prop-types'
 
 import * as S from './styles'
 
-const Link = ({ children, to }) => <S.Link to={to}>{children}</S.Link>
+const Link = ({ children, to, language }) => (
+  <S.Link to={to} language={language}>
+    {children}
+  </S.Link>
+)
 
 Link.propTypes = {
   to: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
+  language: PropTypes.string,
+}
+
+Link.defaultProps = {
+  language: undefined,
 }
 
 export default Link
